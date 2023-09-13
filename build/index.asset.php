71fb866ca4d5a6190d564228a6fd6464cbf6bb11
Bridget Wessel <bridget@unboxinteractive.com>
2023-09-13 10:34:29 -0500

    Fixing icon

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'bc2af3e4202ff680613d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '666082f8ed50b7b35cff');
