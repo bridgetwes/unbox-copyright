4b944c058e8f0e7088ac87a9f5b6c5e41ab0a849
Bridget Wessel <bridget@unboxinteractive.com>
2025-01-30 19:17:51 -0600

    Fixing plugin update checks

--- plugin-update-checker.php
+++ plugin-update-checker.php
@@ -1,20 +1,21 @@
 <?php
-require_once plugin_dir_path( __FILE__ ) . 'vendor/yahnis-elsts/plugin-update-checker/plugin-update-checker.php';
+$plugin_update_checker_path = plugin_dir_path( __FILE__ ) . 'vendor/yahnis-elsts/plugin-update-checker/plugin-update-checker.php';
+
+if (file_exists($plugin_update_checker_path)) {
+    require_once $plugin_update_checker_path;
+} 
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
 function unbox_copyright_plugin_updater() {
     if ( class_exists( 'YahnisElsts\PluginUpdateChecker\v5\PucFactory' ) ) {
         $myUpdateChecker = PucFactory::buildUpdateChecker(
             'https://github.com/bridgetwes/unbox-copyright/',
-            __FILE__, // Full path to the main plugin file or functions.php.
+            plugin_dir_path( __FILE__ ) . 'unbox-copyright.php', // Change to point to main plugin file
             'unbox-copyright'
         );
         
-        // Optional: If you're using a private repository, specify the access token:
-        // $myUpdateChecker->setAuthentication('your-token-here');
-        
         // Set the branch that contains the stable release.
-        $myUpdateChecker->setBranch('main');
+        $myUpdateChecker->setBranch('main');        
     }
 }
 add_action('init', 'unbox_copyright_plugin_updater'); 
