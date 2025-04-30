f857ed51e6d317791a719cbb22bc536985215ce2
Bridget Wessel <bridget@unboxinteractive.com>
2025-04-30 09:49:12 -0500

    Removed color on label so will pick up color of any container's text color

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ee1906d939e765a5a0aa');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '0814655ca786c245e2d1');
